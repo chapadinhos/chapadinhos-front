a0f0b1f441dfa03c953a8a731f665d0d9bb43818
joice <anunes.joice@gmail.com>
2024-02-28 17:47:35 -0300

    feat: using componentes on access pages

--- src/app/access/page.tsx
+++ src/app/access/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import AccessLayout from "@/components/AccessLayout";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function Page() {
+  const router = useRouter();
   const logged = useState(false);
 
   if (logged) {
@@ -16,24 +20,15 @@ export default function Page() {
       subtitle="Accesar seu feed de desafios fitness"
     >
       <form>
-        <input
-          className="mb-3 w-full rounded-md border border-gray-400 px-3 py-2"
-          type="email"
-          placeholder="E-mail"
-        />
-        <input
-          className="mb-2 w-full rounded-md border border-gray-400 px-3 py-2"
-          type="password"
-          placeholder="Senha"
-        />
-        <span>Esqueceu seu e-mail?</span>
+        <Input type="email" placeholder="E-mail" />
+        <Input type="password" placeholder="Senha" />
+        <Link href="/">Esqueceu seu e-mail?</Link>
         <div className="mt-10 flex w-full columns-2 gap-8">
-          <button className="w-full rounded-xl bg-gray-800 px-4 py-2 font-bold text-white hover:bg-gray-600">
-            Criar conta
-          </button>
-          <button className="w-full rounded-xl bg-gray-800 px-4 py-2 font-bold text-white hover:bg-gray-600">
-            Login
-          </button>
+          <Button
+            text="Criar conta"
+            onClick={() => router.push("/access/signup")}
+          />
+          <Button text="Login" onClick={() => router.push("/dashboard")} />
         </div>
       </form>
     </AccessLayout>
--- src/app/access/signup/page.tsx
+++ src/app/access/signup/page.tsx
@@ -1,28 +1,16 @@
 import AccessLayout from "@/components/AccessLayout";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 
 export default function Page() {
   return (
     <AccessLayout title="Criar uma conta" subtitle="Insira suas lorem ipsum">
       <form>
-        <input
-          className="mb-3 w-full rounded-md border border-gray-400 px-3 py-2"
-          type="text"
-          placeholder="Nome"
-        />
-        <input
-          className="mb-3 w-full rounded-md border border-gray-400 px-3 py-2"
-          type="email"
-          placeholder="E-mail"
-        />
-        <input
-          className="mb-2 w-full rounded-md border border-gray-400 px-3 py-2"
-          type="password"
-          placeholder="Senha"
-        />
+        <Input type="text" placeholder="Nome" />
+        <Input type="email" placeholder="E-mail" />
+        <Input type="password" placeholder="Senha" />
         <div className="mt-12 flex w-full">
-          <button className="w-full rounded-xl bg-gray-800 px-4 py-2 font-bold text-white hover:bg-gray-600">
-            Avançar
-          </button>
+          <Button text="Avançar" />
         </div>
       </form>
     </AccessLayout>
