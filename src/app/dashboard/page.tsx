fe95554449c45937196df29babf0dd9602b68eaa
joice <anunes.joice@gmail.com>
2024-03-01 11:08:24 -0300

    feat: experimenting with challenge layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,6 +12,33 @@ export default function Page() {
       </div>
 
       <h4 className="text-h4 font-bold">Desafios ativos</h4>
+      {/* flex */}
+      <div className="-mx-4 flex w-full flex-wrap justify-between gap-y-8">
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        <div className="mx-4 h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+      </div>
+
+      {/* grid */}
+      <div className="w-full">
+        <div className="grid-cols-auto-fill-200 grid grid-flow-col gap-8">
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+          <div className="h-[200px] w-[200px] rounded-[20px] bg-white drop-shadow-md"></div>
+        </div>
+      </div>
     </div>
   );
 }
