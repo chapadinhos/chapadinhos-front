2d51da0cc3f6b348e2d7768a42124dda974542d2
joice <anunes.joice@gmail.com>
2024-03-01 10:55:57 -0300

    feat: dashboard page structure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+export default function Page() {
+  return (
+    <div className="p-10">
+      <h4 className="text-h4 font-bold">Convites</h4>
+      <div className="h-[230px] w-full rounded-[20px] bg-white drop-shadow-md"></div>
+      <div className="flex w-full place-content-center">
+        <div className="mx-1 my-4 h-2 w-2 rounded-full bg-gray-500"></div>
+        <div className="mx-1 my-4 h-2 w-2 rounded-full bg-gray-300"></div>
+        <div className="mx-1 my-4 h-2 w-2 rounded-full bg-gray-300"></div>
+        <div className="mx-1 my-4 h-2 w-2 rounded-full bg-gray-300"></div>
+        <div className="mx-1 my-4 h-2 w-2 rounded-full bg-gray-300"></div>
+      </div>
+
+      <h4 className="text-h4 font-bold">Desafios ativos</h4>
+    </div>
+  );
+}
